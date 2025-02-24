67d98d8fb8f6cb944ab67c70edd3fa0a25875e0c
AdrianOKD <adrian.karl.oskar.dahl@gmail.com>
2025-02-24 15:18:18 +0100

    Update

--- Js/popupPostWindow.js
+++ Js/popupPostWindow.js
@@ -2,9 +2,19 @@ document.addEventListener('DOMContentLoaded', () => {
     console.log("DOM fully loaded and parsed");
     const createPostButton = document.querySelector('.create-post-button');
     const createPostPopup = document.getElementById('createPostPopup');
+    const closeButton = document.querySelector('.close-button');
+    const createPostForm = document.getElementById('createPostForm');
 
     closeButton.addEventListener('click', () => {
         createPostPopup.style.display = 'none';
+      });
+      createPostButton.addEventListener('click', () => {
+        createPostButton.style.display = 'block';
+      });
+      window.addEventListener('click', (event) => {
+        if (event.target === popup) {
+          createPostButton.style.display = 'none';
+        }
       });
     
 })
