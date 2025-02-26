a58057d358940ae33655363a901cb5bd13ac589f
AdrianOKD <adrian.karl.oskar.dahl@gmail.com>
2025-02-26 09:36:46 +0100

    Fix: Wrong syntax

--- Js/popupPostWindow.js
+++ Js/popupPostWindow.js
@@ -6,28 +6,31 @@ document.addEventListener('DOMContentLoaded', () => {
     const createPostForm = document.getElementById('createPostForm');
 
     closeButton.addEventListener('click', () => {
-        createPostPopup.style.display = 'none';
-      });
-      createPostButton.addEventListener('click', () => {
-        createPostButton.style.display = 'block';
-      });
-      window.addEventListener('click', (event) => {
-        if (event.target === popup) {
-          createPostButton.style.display = 'none';
-        }
-        createPostForm.addEventListener('submit', (event) => {
-            event.preventDefault();
-            
-            const formData = {
-              title: document.getElementById('postTitle').value,
-              body: document.getElementById('postBody').value,
-              tags: document.getElementById('postTags').value.split(',').map(tag => tag.trim())
-            };
-        
-            console.log('New post data:', formData);
-            
-            createPostForm.reset();
-            popup.style.display = 'none';
-      });
+      createPostPopup.style.display = 'none';
+    });
     
-})
+    createPostButton.addEventListener('click', () => {
+      createPostPopup.style.display = 'block';
+    });
+    
+    window.addEventListener('click', (event) => {
+      if (event.target === createPostPopup) {
+        createPostPopup.style.display = 'none';
+      }
+    });
+  
+    createPostForm.addEventListener('submit', (event) => {
+      event.preventDefault();
+      
+      const formData = {
+        title: document.getElementById('postTitle').value,
+        body: document.getElementById('postBody').value,
+        tags: document.getElementById('postTags').value.split(',').map(tag => tag.trim())
+      };
+  
+      console.log('New post data:', formData);
+      
+      createPostForm.reset();
+      createPostPopup.style.display = 'none';
+    });
+  });
