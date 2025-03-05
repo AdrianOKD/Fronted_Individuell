c5c22e810ac28f17cc725cc0b0ca2033ca13c92a
AdrianOKD <adrian.karl.oskar.dahl@gmail.com>
2025-03-05 10:24:34 +0100

    Add: User name and lastname

--- Js/user.js
+++ Js/user.js
@@ -3,6 +3,8 @@ export class User{
     constructor(id,username ){
         this.id = id;
         this.username = username;
+        this.firstName = firstName;
+        this.lastName = lastName;
        
     }
 }
