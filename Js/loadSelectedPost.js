00d5178169de8a4063f2ff205667cde79068b187
AdrianOKD <adrian.karl.oskar.dahl@gmail.com>
2025-03-22 20:09:02 +0100

    Add: Function for comments on Selected Post.

--- Js/loadSelectedPost.js
+++ Js/loadSelectedPost.js
@@ -1,3 +1,4 @@
+import { fetchComments } from "./apiClient";
 import { createPostElement } from "./main.js";
 
 document.addEventListener('DOMContentLoaded', () => {
@@ -18,3 +19,23 @@ function loadSelectedPost() {
 
     container.appendChild(selectedPost);
 }
+function loadSelectedPostComments()
+{
+    const post = JSON.parse(localStorage.getItem('selectedPost'))
+
+
+   const commentSection = document.createElement('div');
+   commentSection.id = 'comment-section'
+
+   const commentsList  = document.createElement('div')
+   commentList.id = 'commentList';
+   commentSection.appendChild(commentsList);
+
+
+}
+
+document.getElementById('select-post-container').appendChild(commentSection);
+
+fetchComments().then(response => {
+    const selectedPostComments = response.comments.filter(comment => comment.postId === post.id);
+}) 
